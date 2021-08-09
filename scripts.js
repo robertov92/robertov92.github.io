ba3866397abe32a424f5713bfaef1137bc1e0c55
robertov92 <vil13004@byui.edu>
2021-08-09 11:51:51 -0500

    firts commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', () => {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+window.addEventListener('scroll', () => {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+});
